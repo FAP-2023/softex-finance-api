2600a96d91669b02283c5e4fd565b967afafa67d
Lucas-E <lucasgitacc@gmail.com>
2023-11-22 21:18:33 -0300

    adicionei configuração de camadas

--- src/app.ts
+++ src/app.ts
@@ -5,6 +5,8 @@ import 'dotenv/config'
 function startApp(){
     try {
         const app = express();
+        app.use(express.json());
+        app.use(express.urlencoded({extended:true}))
         const server = createServer(app);
         server.listen(process.env.PORT, () => {
             console.log('Servidor rodando na porta', process.env.PORT)
