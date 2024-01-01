cde917aa198ba7733b638fd17e25a28940e96569
Lucas Silva <lucasgitacc@gmail.com>
2024-01-01 16:01:41 -0300

    configurando eslint

--- .eslintrc.cjs
+++ .eslintrc.cjs
@@ -4,4 +4,10 @@ module.exports = {
   parser: "@typescript-eslint/parser",
   plugins: ["@typescript-eslint"],
   root: true,
+  rules: {
+    // ... outras regras ...
+
+    // Permitir o uso do tipo 'any' em situações específicas
+    "@typescript-eslint/no-explicit-any": "off"
+  }
 };
