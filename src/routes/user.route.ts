4ea1959b915e93e4dd984d4d82c033dd68e25c54
Lucas Silva <lucasgitacc@gmail.com>
2024-01-02 01:37:34 -0300

    adicionando rotas

--- src/routes/user.route.ts
+++ src/routes/user.route.ts
@@ -1,11 +1,13 @@
 import { Router } from "express";
 import { userController } from "../factories/user.factory";
 
-
 export const UserRoutes = (): Router => {
-    const router = Router();
+	const router = Router();
 
-    router.post("/", userController.createrUser);
-
-    return router;
+	router.post("/", (req, res) => userController.createrUser(req, res));
+	router.get("/:id", (req, res) => userController.getUserById(req, res));
+    router.get("/:email", (req, res) => userController.getUserByEmail(req, res))
+    router.put("/", (req, res) => userController.updateUser(req, res))
+    router.delete("/:id", (req, res) => userController.deleteUser(req, res))
+	return router;
 };
