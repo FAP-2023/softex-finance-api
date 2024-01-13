d4b05d1aa2eb1905cd26a56244ce3f9c694ff710
Lucas Silva <lucasgitacc@gmail.com>
2024-01-13 11:10:03 -0300

    adicionando cascade

--- src/entites/user.entity.ts
+++ src/entites/user.entity.ts
@@ -2,37 +2,37 @@ import { Column, Entity, OneToMany, PrimaryGeneratedColumn } from "typeorm";
 import { AbstractEntity } from "./AbstractEntity";
 import { Customer } from "./Customer.entity";
 
-
 @Entity("users")
-export class User extends AbstractEntity{
+export class User extends AbstractEntity {
+	constructor(email: string, password: string, name: string) {
+		super();
+		this.name = name;
+		this.email = email;
+		this.password_hash = password;
+	}
 
-    constructor(email:string, password:string, name:string){
-        super()
-        this.name = name;
-        this.email = email;
-        this.password_hash = password;
-    }
+	@PrimaryGeneratedColumn()
+	id: number;
 
-    @PrimaryGeneratedColumn()
-    id:number;
+	@Column({
+		nullable: false,
+	})
+	name: string;
 
-    @Column({
-        nullable:false
-    })
-    name:string;
+	@Column({
+		unique: true,
+		nullable: false,
+	})
+	email: string;
 
-    @Column({
-        unique:true,
-        nullable:false
-    })
-    email:string;
+	@Column({
+		nullable: false,
+	})
+	password_hash: string;
 
-    @Column({
-        nullable:false
-    })
-    password_hash:string;
-
-    @OneToMany(() => Customer, (customer) => customer.user)
-    customers: Customer[];
+	@OneToMany(() => Customer, (customer) => customer.user, {
+		cascade: true,
+		onDelete: "CASCADE",
+	})
+	customers: Customer[];
 }
-
